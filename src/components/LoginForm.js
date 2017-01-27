fe53e74090551f02543808653ab2ff5dfec29d00
Andrew Merrell <andrew.m.merrell@gmail.com>
2017-01-27 11:53:32 -0800

    add props to password CardSection to display password field.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state={ text: '' };
+  state={ email: '' };
+  state={ password: '' };
 
   render() {
     return (
       <Card>
         <CardSection>
           <Input
+            placeholder="username@email.com"
             label="Email"
-            value={this.state.text}
-            onChangeText={text => this.setState({ text })}
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
             underlineColorAndroid={'transparent'}
           />
         </CardSection>
-        <CardSection />
+        
+        <CardSection>
+          <Input
+            placeholder="password"
+            label="Password"
+            secureTextEntry
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+            underlineColorAndroid={'transparent'}
+          />
+        </CardSection>
 
         <CardSection>
           <Button>
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, value, onChangeText }) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
   const { inputStyle, labelStyle, containerStyle } = styles;
 
   return (
     <View style={containerStyle}>
-      <Text styl={labelStyle}>{label}</Text>
+      <Text style={labelStyle}>{label}</Text>
       <TextInput
+        secureTextEntry={secureTextEntry}
+        placeholder={placeholder}
+        autoCorrect={false}
         style={inputStyle}
         value={value}
         onChangeText={onChangeText}
@@ -27,6 +30,7 @@ const styles = {
     flex: 2
   },
   labelStyle: {
+    color: '#000',
     fontSize: 18,
     paddingLeft: 20,
     flex: 1
@@ -37,6 +41,6 @@ const styles = {
     flexDirection: 'row',
     alignItems: 'center'
   }
-}
+};
 
 export { Input };
